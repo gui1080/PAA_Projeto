426a0378181159024ad46f3ab4e72fc7d46a6f8e
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-10-23 17:07:12 -0300

    Implementação final do scraper

--- scrapy/scraper.py
+++ scrapy/scraper.py
@@ -1,9 +1,9 @@
 import os
-import scrapy 
+import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.http import Request
 
-from criador_url import pega_url 
+from criador_url import pega_url
 from salva_pagina import salva_pagina
 
 fb_pages = 0
@@ -15,22 +15,59 @@ class firstSpider(scrapy.Spider):
         os.mkdir("./downloaded_pages")
     except FileExistsError:
         pass
-    
+
     name = "basic"
     start_urls = pega_url()
-  
+
+    custom_settings = {
+        'DEFAULT_REQUEST_HEADERS': {
+            'Accept-Language': 'pt'
+        }
+    }
 
     def parse(self, response):
         global fb_pages, MAX_FB_PAGES
-        
+
+        if response.status != 200:
+            return
+
+        res_lang: str = None
+        res_type: str = None
+
+        for (header, value) in response.headers.items():
+            header_name = header.decode().lower()
+            if(header_name == "content-language"):
+                res_lang = value[0].decode()
+
+                if "pt" not in res_lang and "en" not in res_lang:
+                    return
+
+                if res_type != None:
+                    break
+
+            elif (header_name == "content-type"):
+                res_type = value[0].decode()
+
+                if "text/html" not in res_type:
+                    return
+
+                if res_lang != None:
+                    break
+
+        if res_lang == None and ".br" not in response.url:
+            return
+
         xlink = LinkExtractor()
         salva_pagina(response)
-        
+
         for link in xlink.extract_links(response):
             if "facebook" in link.url:
                 if fb_pages > MAX_FB_PAGES:
                     continue
                 fb_pages += 1
-            
-            print(link.text, "\n", link.url, "\n")
+
+            if "docs.github.com" in link.url:
+                continue
+
+            # print(link.text, "\n", link.url, "\n")
             yield Request(link.url)
